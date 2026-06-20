4c02531774f30775938009efca78bf4b9bf7e879
teemo8400-lang <teemo8400@gmail.com>
2026-06-21 02:20:25 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
 app.get("/api/health", (req, res) => {
   res.json({ ok: true });
